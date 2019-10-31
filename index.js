619c1ca6a771653b5935cc218f99d90880120b26
Kai Lawrence <mail@kailawrence.de>
2019-10-31 21:06:17 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const hash = window.location.hash;
-if (hash) {
-    document.getElementById('links').style.display = 'none';
-    const text = document.createElement('h1');
-    const a = document.getElementById(hash);
-    text.innerHTML = `You are being redirected to: ${a.href}`;
-    document.body.appendChild(text);
-    setTimeout(() => { a.click(); }, 3000);
-}
